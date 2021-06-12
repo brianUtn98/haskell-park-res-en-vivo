c4641c83664327c2a6d5b3f1ac763c01ec5d6497
Brian Monroy <brian.gmonroy98@gmail.com>
2021-06-12 15:43:34 -0300

    Segundo commit (casi tercero)

--- proyecto-test/src/Library.hs
+++ proyecto-test/src/Library.hs
@@ -15,6 +15,8 @@ data Persona = Persona {
 
 juan = Persona "Juan" 20 30 40
 ana = Persona "Ana" 10 20 60
+grinch = Persona "Grinch" 1 150 1
+leopoldo = Persona "Leopoldo" 1 1 1
 
 {-
 *Punto 2 Atracciones
@@ -67,4 +69,44 @@ visitar atracciones persona = foldl (flip($)) persona atracciones
 * Punto 4
 -}
 
+--Mi funcion inventada te deja como, asistieses a un showDeMagia y luego a una montaña rusa de 100km/h y 50mts
 
+atraccionInventada = (\persona -> (montañaRusa 100 50).showDeMagia $ persona)
+{-
+*Library> visitar [montañaRusa 20 20,caidaLibre 50, mundoMaya, showDeMagia,(\persona -> (montañaRusa 100 50).showDeMagia $ persona)] ana
+Persona {nombre = "Ana", satisfaccion = 29, emocion = 100.09, cultura = 86.4}
+-}
+todas = [montañaRusa 20 20,caidaLibre 50, mundoMaya, showDeMagia,(\persona -> (montañaRusa 100 50).showDeMagia $ persona)]
+{-
+visitar todas ana
+Persona {nombre = "Ana", satisfaccion = 29, emocion = 100.09, cultura = 86.4}
+-}
+
+{-
+* Punto 5
+-}
+
+estaSatisfecha = (>50).satisfaccion
+estaEmocionada = (>60).emocion
+
+estanFelices :: [Persona] -> Bool
+estanFelices personas = all (estaSatisfecha).filter (estaEmocionada.mundoMaya.montañaRusa 80 10) $ personas
+
+{-
+* Punto 6
+-}
+cumpleContenta persona = (>100).(+ emocion persona).satisfaccion $ persona
+
+estaContenta :: [Atraccion] -> Persona -> Bool
+estaContenta atracciones persona = cumpleContenta.visitar atracciones $ persona
+
+{-
+* Punto 7
+-}
+--a No se puede, nunca terminaría ya que requiere procesar toda la lista para aplicar el fold.
+atraccionesInfinitas = mundoMaya : atraccionesInfinitas
+--b
+h f xs = (head.filter f) xs
+
+personasInfinitas = ana: personasInfinitas
+--Se puede gracias a la evaluación diferida de Haskell, pero depende de si en la lista infinita de personas hay una que cumpla la condición de estar contenta.
--- proyecto-test/src/Spec.hs
+++ proyecto-test/src/Spec.hs
@@ -31,35 +31,35 @@ correrTests = hspec $ do
     describe "visitar" $ do
       it "emocion de persona que visita una atraccion" $ do
         emocion (visitar [montañaRusa 60 20] ana) `shouldBe` emocion (montañaRusa 60 20 ana) 
-      -- it "emocion de persona que visita muchas atracciones" $ do
-      --   emocion (visitar todas ana) `shouldBe` emocion ((\persona -> (montañaRusa 100 50).showDeMagia $ persona) . showDeMagia . mundoMaya . caidaLibre 50 . montañaRusa 20 20 $ ana)
+      it "emocion de persona que visita muchas atracciones" $ do
+        emocion (visitar todas ana) `shouldBe` emocion ((\persona -> (montañaRusa 100 50).showDeMagia $ persona) . showDeMagia . mundoMaya . caidaLibre 50 . montañaRusa 20 20 $ ana)
       it "emocion de persona que visita ninguna atraccion" $ do
         emocion (visitar [] ana) `shouldBe` emocion ana
 
-  -- describe "Atraccion inventada" $ do
-  --   describe "atraccion inventada" $ do
-  --     it "atraccion inventada es como ir a un show de magia y luego a una montaña rusa de 50 metros a 100km/h" $ do
-  --       emocion (atraccionInventada ana) `shouldBe` emocion (montañaRusa 100 50 . showDeMagia $ ana)
+  describe "Atraccion inventada" $ do
+    describe "atraccion inventada" $ do
+      it "atraccion inventada es como ir a un show de magia y luego a una montaña rusa de 50 metros a 100km/h" $ do
+        emocion (atraccionInventada ana) `shouldBe` emocion (montañaRusa 100 50 . showDeMagia $ ana)
   
-  -- describe "estan felices" $ do
-  --   describe "estan felices" $ do
-  --     it "personas que estan felices" $ do
-  --       estanFelices [ana,juan] `shouldBe` True
-  --     it "no todas estan felices" $ do
-  --       estanFelices [ana,juan,grinch] `shouldBe` False
-  --     it "estan felices una lista vacia" $ do
-  --       estanFelices [] `shouldBe` True
+  describe "estan felices" $ do
+    describe "estan felices" $ do
+      it "personas que estan felices" $ do
+        estanFelices [ana,juan] `shouldBe` True
+      it "no todas estan felices" $ do
+        estanFelices [ana,juan,grinch] `shouldBe` False
+      it "estan felices una lista vacia" $ do
+        estanFelices [] `shouldBe` True
   
-  -- describe "esta contenta" $ do
-  --   describe "esta contenta" $ do
-  --     it "persona esta contenta al visitar una atraccion" $ do
-  --       estaContenta [montañaRusa 100 100] ana `shouldBe` True
-  --     it "persona esta contenta al visitar todas las atracciones" $ do
-  --       estaContenta todas grinch `shouldBe` True
-  --     it "persona no esta contenta al visitar una atraccion" $ do
-  --       estaContenta [mundoMaya] leopoldo `shouldBe` False
-  --     it "persona no esta contenta al visitar varias atracciones" $ do
-  --       estaContenta [montañaRusa 20 20,caidaLibre 10,mundoMaya] leopoldo `shouldBe` False
+  describe "esta contenta" $ do
+    describe "esta contenta" $ do
+      it "persona esta contenta al visitar una atraccion" $ do
+        estaContenta [montañaRusa 100 100] ana `shouldBe` True
+      it "persona esta contenta al visitar todas las atracciones" $ do
+        estaContenta todas grinch `shouldBe` True
+      it "persona no esta contenta al visitar una atraccion" $ do
+        estaContenta [mundoMaya] leopoldo `shouldBe` False
+      it "persona no esta contenta al visitar varias atracciones" $ do
+        estaContenta [montañaRusa 20 20,caidaLibre 10,mundoMaya] leopoldo `shouldBe` False
 
   -- describe "personas infinitas" $ do
   --   describe "personas infinitas" $ do
